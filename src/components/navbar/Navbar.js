5d0771a24d9c19cfefb63e91ac195099f58e8c19
Erick3017 <erickvinicio30@gmail.com>
2023-08-18 23:17:11 -0600

    commit navbar

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,28 @@
+
+import React from "react";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from '../../context/AuthContext';
+import "./navbar.css"
+
+export default function Navbar() {
+    const { user } = useContext(AuthContext);    
+    return (
+        <div className="navbar">
+            <div className="navContainer">
+                <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+                    <span className="logo">{user.username}</span>
+                </Link>
+                <div className="navItems">
+                    {user.isAdmin ? (
+                        <button className="navButton">
+                            <Link to="/users" style={{ color: "inherit", textDecoration: "none" }}>
+                                <span className="logo">Users</span>
+                            </Link>
+                        </button>
+                    ) : ""}
+                </div>
+            </div>
+        </div>
+    );
+}
